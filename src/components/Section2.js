fcfc5c1fc1177e90efdc6aca551e847b24450405
JunYoung <jun094@naver.com>
2020-02-12 21:40:47 +0900

    Section2 틀잡기

---
+++ src/components/Section2.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../styles/section2.scss';
+
+function Section2() {
+    return <></>;
+}
+
+export default Section2;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Section1 from '../components/Section1';
+import Section2 from '../components/Section2';
 import Section3 from '../components/Section3';
 
 function Main() {
     return (
         <>
             <Section1></Section1>
+            <Section2></Section2>
             <Section3></Section3>
         </>
     );
