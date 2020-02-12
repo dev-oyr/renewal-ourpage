875251bf54ba665db4d7f6d4abcee86e8450c272
Sain <chyh1900@gmail.com>
2020-02-12 21:51:02 +0900

    로컬 폰트 적용

--- src/App.js
+++ src/App.js
@@ -5,11 +5,6 @@ import Main from './pages/Main';
 function App() {
     return (
         <>
-            {/* google web font Noto Sans */}
-            <link href="https://fonts.googleapis.com/css?family=Noto+Sans+KR&display=swap" rel="stylesheet"></link>
-            {/* google web font Montserrat */}
-            <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap" rel="stylesheet" />
-
             <Main></Main>
         </>
     );
