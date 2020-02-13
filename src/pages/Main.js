b9be77ce0f7fb7ba4a71e3e40c3709aa5e571ac8
Sain <chyh1900@gmail.com>
2020-02-14 00:13:51 +0900

    연혁 컴포넌트 생성
    배열 동적 렌더링 추가 예정

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Section1 from '../components/Section1';
 import Section2 from '../components/Section2';
 import Section3 from '../components/Section3';
+import Section4 from '../components/Section4';
 
 function Main() {
     return (
@@ -9,6 +10,7 @@ function Main() {
             <Section1></Section1>
             <Section2></Section2>
             <Section3></Section3>
+            <Section4></Section4>
         </>
     );
 }
