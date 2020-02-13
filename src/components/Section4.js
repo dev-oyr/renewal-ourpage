00539cd8da5b9b2a046a972f0f6611d2f8e9d170
Sain-Tech <chyh1900@gmail.com>
2020-02-14 07:05:08 +0800

    연혁 섹션 구현
    추후 데이터 json화 및 불러오는 기능 구현 예정

--- src/components/Section4.js
+++ src/components/Section4.js
@@ -3,42 +3,22 @@ import Fade from 'react-reveal/Fade';
 import '../styles/section4.scss';
 
 const histories = {
-    '2014': [
+    '2018': [
         {
-            title: 'WeatherMusic',
-            content: '현재 날씨에 어울리는 음악추천 WeatherMusic, 2014 청년창업기상기후산업 기상청장상',
+            title: 'Schedury',
+            content: '한 눈에 일정 및 일기를 작성하고 관리할 수 있는 Schedury, 교내 학술제 입상',
         },
         {
-            title: '오늘의 자외선',
-            content: '오늘의 자외선, 기상청 추천 앱 등록',
+            title: 'InMirror',
+            content: '외출 준비를 할 때 거울을 보며 각종 정보들을 알 수 있는 스마트 거울 InMirror, 교내 학술제 입상',
         },
         {
-            title: '그래서 오늘은',
-            content: '뉴스 요약 어플 그래서 오늘은, 2014 정주영 창업경진대회 해커톤 2위',
+            title: '시장올래',
+            content: '서울시 재래시장 활성화 웹 애플리케이션 시장올래, 2018 서울시 앱 공모전 2위',
         },
         {
-            title: 'Fethering',
-            content: '대학생 이상형 월드컵 Fethering, venture square 2014 5월의 아이템 선정',
-        },
-    ],
-    '2015': [
-        {
-            title: 'Keepet',
-            content: '동물병원 추천 플랫폼 Keepet, 2015 교내 학술제 입상',
-        },
-        {
-            title: '아뭐듣지',
-            content: '강의평가 및 시간표 짜기 어플리케이션 아뭐듣지 (등록된 시간표 수 1400개)',
-        },
-    ],
-    '2016': [
-        {
-            title: 'PricePlace',
-            content: '서울시 물가비교 쇼핑 어플 PricePlace, 2016 교내 학술제 입상',
-        },
-        {
-            title: 'TabStorage',
-            content: '브라우저 탭 통합 서비스 TabStorage, 2016 교내 학술제 입상',
+            title: 'Sejong Spec Up',
+            content: '세종대학생들의 취업 스펙 비교 및 자신의 위치를 알려주는 Sejong Spec Up, 교내 해커톤 입상',
         },
     ],
     '2017': [
@@ -63,22 +43,42 @@ const histories = {
             content: 'AR보행자 네비게이션 뚜벅이, 2017 세종 창업 아이디어 경진대회 장려상',
         },
     ],
-    '2018': [
+    '2016': [
         {
-            title: 'Sejong Spec Up',
-            content: '세종대학생들의 취업 스펙 비교 및 자신의 위치를 알려주는 Sejong Spec Up, 교내 해커톤 입상',
+            title: 'PricePlace',
+            content: '서울시 물가비교 쇼핑 어플 PricePlace, 2016 교내 학술제 입상',
         },
         {
-            title: '시장올래',
-            content: '서울시 재래시장 활성화 웹 애플리케이션 시장올래, 2018 서울시 앱 공모전 2위',
+            title: 'TabStorage',
+            content: '브라우저 탭 통합 서비스 TabStorage, 2016 교내 학술제 입상',
+        },
+    ],
+    '2015': [
+        {
+            title: 'Keepet',
+            content: '동물병원 추천 플랫폼 Keepet, 2015 교내 학술제 입상',
         },
         {
-            title: 'InMirror',
-            content: '외출 준비를 할 때 거울을 보며 각종 정보들을 알 수 있는 스마트 거울 InMirror, 교내 학술제 입상',
+            title: '아뭐듣지',
+            content: '강의평가 및 시간표 짜기 어플리케이션 아뭐듣지 (등록된 시간표 수 1400개)',
+        },
+    ],
+    '2014': [
+        {
+            title: 'WeatherMusic',
+            content: '현재 날씨에 어울리는 음악추천 WeatherMusic, 2014 청년창업기상기후산업 기상청장상',
         },
         {
-            title: 'Schedury',
-            content: '한 눈에 일정 및 일기를 작성하고 관리할 수 있는 Schedury, 교내 학술제 입상',
+            title: '오늘의 자외선',
+            content: '오늘의 자외선, 기상청 추천 앱 등록',
+        },
+        {
+            title: '그래서 오늘은',
+            content: '뉴스 요약 어플 그래서 오늘은, 2014 정주영 창업경진대회 해커톤 2위',
+        },
+        {
+            title: 'Fethering',
+            content: '대학생 이상형 월드컵 Fethering, venture square 2014 5월의 아이템 선정',
         },
     ],
 };
@@ -87,24 +87,34 @@ function HistoryGroup({ year, contents }) {
     return (
         <div className="history-group">
             <div className="desc-font en text-year">
-                <Fade left distance="50%" fraction={0.7}>
-                    <p>2019</p>
+                <Fade left distance="60%" fraction={0.7}>
+                    <p>{year}</p>
                 </Fade>
             </div>
             <div className="history-elements">
-                <Fade left distance="50%" fraction={1}>
-                    <p className="kr text-history">제 6회 컴퓨터공학과 학술제 입상(장려상)</p>
-                </Fade>
-                <Fade left distance="50%" fraction={1}>
-                    <p className="kr text-history">캡스톤 2등했다.</p>
-                </Fade>
-                <Fade left distance="50%" fraction={1}>
-                    <p className="kr text-history">Open Source S/W 대회 출전</p>
-                </Fade>
+                {contents.map(({ title, content }, k) => (
+                    <Fade left distance="15%" fraction={1} key={k}>
+                        <p className="kr text-history">
+                            {content.substring(0, content.indexOf(title))}
+                            <span>{title}</span>
+                            {content.substring(content.indexOf(title) + title.length, content.length)}
+                        </p>
+                    </Fade>
+                ))}
             </div>
         </div>
     );
 }
+
+HistoryGroup.defaultProps = {
+    year: new Date().getFullYear(),
+    contents: [
+        {
+            title: '여기에 타이틀이 나타납니다.',
+            content: '여기에 내용이 나타납니다.',
+        },
+    ],
+};
 
 function Section4() {
     return (
@@ -116,11 +126,11 @@ function Section4() {
                     </Fade>
                 </div>
                 <div className="inner-content">
-                    <HistoryGroup></HistoryGroup>
-                    <HistoryGroup></HistoryGroup>
-                    <HistoryGroup></HistoryGroup>
-                    <HistoryGroup></HistoryGroup>
-                    <HistoryGroup></HistoryGroup>
+                    {Object.keys(histories)
+                        .reverse()
+                        .map((y, k) => (
+                            <HistoryGroup year={y} contents={histories[y]} key={k}></HistoryGroup>
+                        ))}
                 </div>
             </div>
         </div>
