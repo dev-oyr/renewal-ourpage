48bececc8bedc2c8b25723647c648733ee0e3ad8
Sain Choi <chyh1900@gmail.com>
2020-02-14 23:41:15 +0900

    소개페이지 섹션 2 소개, 스터디, 홈커밍 구현 (#8)
    * Update App.js
    
    * 연혁 컴포넌트 생성
    
    배열 동적 렌더링 추가 예정
    
    * 연혁 섹션 구현
    
    추후 데이터 json화 및 불러오는 기능 구현 예정
    
    * 애니메이션 및 스타일 변경
    
    애니메이션 거리, 연혁 그룹 간 간격 조정
    
    * json 데이터 생성
    
    * 소개페이지 섹션 2 소개, 스터디, 홈커밍 설명 구현

---
+++ src/components/Section4.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import Fade from 'react-reveal/Fade';
+import '../styles/section4.scss';
+import histories from '../datas/histories.json';
+
+function HistoryGroup({ year, contents }) {
+    return (
+        <div className="history-group">
+            <div className="desc-font en text-year">
+                <Fade left distance="48px" fraction={0.7} duration={666}>
+                    <p>{year}</p>
+                </Fade>
+            </div>
+            <div className="history-elements">
+                {contents.map(({ title, content }, k) => (
+                    <Fade left distance="72px" fraction={1} duration={666} key={k}>
+                        <p className="kr text-history">
+                            {content.substring(0, content.indexOf(title))}
+                            <span>{title}</span>
+                            {content.substring(content.indexOf(title) + title.length, content.length)}
+                        </p>
+                    </Fade>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+HistoryGroup.defaultProps = {
+    year: new Date().getFullYear(),
+    contents: [
+        {
+            title: '여기에 타이틀이 나타납니다.',
+            content: '여기에 내용이 나타납니다.',
+        },
+    ],
+};
+
+function Section4() {
+    return (
+        <div className="section4">
+            <div className="responsive">
+                <div className="inner-header">
+                    <Fade bottom distance="108px" fraction={1}>
+                        <h1 className="header-font kr">동아리 연혁</h1>
+                    </Fade>
+                </div>
+                <div className="inner-content">
+                    {Object.keys(histories)
+                        .reverse()
+                        .map((y, k) => (
+                            <HistoryGroup year={y} contents={histories[y]} key={k}></HistoryGroup>
+                        ))}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Section4;
