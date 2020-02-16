d6ec1dff377832e270f1d4a5a597a90cde8e4532
JunYoung <jun094@naver.com>
2020-02-16 12:36:53 +0900

    from Ant Design

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Nav from '../components/Nav';
 import Section1 from '../components/Section1';
 import Section2 from '../components/Section2';
 import Section3 from '../components/Section3';
@@ -7,6 +8,7 @@ import Section4 from '../components/Section4';
 function Main() {
     return (
         <>
+            <Nav></Nav>
             <Section1></Section1>
             <Section2></Section2>
             <Section3></Section3>
