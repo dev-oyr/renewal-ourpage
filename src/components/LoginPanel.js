ec350a0415b67ccab672ebae0d62f44f05ba2db0
eviloliver <evil_oliver@kakao.com>
2020-02-25 00:06:33 +0900

    일단 궁금한거 해결

--- src/components/LoginPanel.js
+++ src/components/LoginPanel.js
@@ -2,41 +2,49 @@ import React, { useState } from 'react';
 import Fade from 'react-reveal/Fade';
 
 function LoginPannel() {
+    let [cnt, setCnt] = useState(0);
     let [account, setAccount] = useState({
         id: '',
         password: '',
         checkId: false,
         checkPassword: false,
     });
-
+    const Plus = () => {
+        setCnt(cnt + 1);
+    };
     const handleChange = e => {
         const name = e.target.name;
         const value = e.target.value;
-        account[name] = value;
-        setAccount(account);
+        account.checkId = !account.checkId;
+
+        setAccount({
+            ...account,
+            [name]: value,
+        });
     };
-    const login = e => {
+    const loginCheck = () => {
         if (account.id.length % 2) {
             //일단 아이디가 홀수일때
+            account.checkId = false;
             if (account.password.length % 2) {
                 //일단 아이디가 홀수이고 비밀번호가 홀수일때
-                e.preventDefault();
             } else {
                 account.checkPassword = true;
-                e.preventDefault();
             }
         } else {
             account.checkId = true;
-            console.log(account);
-            e.preventDefault();
         }
     };
 
     return (
         <div>
-            <form onSubmit={login}>
+            <div>{cnt}</div>
+            <button onClick={Plus}>+</button>
+
+            <form>
                 <input type="text" name="id" placeholder="아이디" onChange={handleChange} />
                 <div>{account.checkPassword}</div>
+                {console.log(account)}
                 <Fade bottom collapse when={account.checkId}>
                     <div className="invalid-feedback" style={{ display: 'block' }}>
                         {'존재하지 않는 아이디 입니다'}
@@ -48,25 +56,12 @@ function LoginPannel() {
                         {'일치하지 않는 비밀번호입니다'}
                     </div>
                 </Fade>
-
-                <input type="submit" value="로그인" />
+                <button type="button" onClick={loginCheck}>
+                    로그인
+                </button>
             </form>
         </div>
     );
 }
 
 export default LoginPannel;
-
-/*
-<form method="post" onSubmit={save}>
-          Username <input type="text" name="username" onChange={handleChange} />
-          <br />
-          Password <input type="password" name="password" onChange={handleChange} />
-          <br />
-          Description
-          <br />
-          <textarea name="description" cols="20" rows="5" onChange={handleChange}></textarea>
-          <br />
-          <input type="submit" value="Save" />
-        </form>
-        */
