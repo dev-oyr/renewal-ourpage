c8a650f805654a4590c91867f1859360b8b8d389
JunYoung <jun094@naver.com>
2020-02-26 18:39:40 +0900

    Accordian 컴포넌트 함수형 컴포넌트로 변경

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,57 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FAQData from '../datas/FAQData.json';
 import '../styles/accordion.scss';
 
-class Accordion extends React.Component {
-    render() {
-        return (
-            <div className="wrapper">
-                <ul className="accordion-list">
-                    {Object.keys(FAQData.data).map(k => {
-                        return (
-                            <li {...{ className: 'accordion-list__item' }}>
-                                <AccordionItem title={FAQData.data[k].title} paragraph={FAQData.data[k].paragraph} />
-                            </li>
-                        );
-                    })}
-                </ul>
+function AccordionItem({ paragraph, title }) {
+    const [opened, setOpened] = useState(false);
+    return (
+        <div
+            {...{
+                className: `accordion-item, ${opened && 'accordion-item--opened'}`,
+                onClick: () => {
+                    setOpened(!opened); //디드 마운트
+                },
+            }}
+        >
+            <div className="accordion-item__line">
+                <h3 className="accordion-item__title">{title}</h3>
+                <span className="accordion-item__icon" />
             </div>
-        );
-    }
+            <div className="accordion-item__inner">
+                <div className="accordion-item__content">
+                    <p className="accordion-item__paragraph">{paragraph}</p>
+                </div>
+            </div>
+        </div>
+    );
 }
-
-class AccordionItem extends React.Component {
-    state = {
-        opened: false, //상태가 변해야 하는 같이기 때문에 state이고 그래서 클래스컴포넌트를 쓴다
-    };
-
-    render() {
-        const {
-            props: { paragraph, title },
-            state: { opened },
-        } = this;
-
-        return (
-            <div
-                {...{
-                    className: `accordion-item, ${opened && 'accordion-item--opened'}`,
-                    onClick: () => {
-                        this.setState({ opened: !opened }); //디드 마운트
-                    },
-                }}
-            >
-                <div className="accordion-item__line">
-                    <h3 className="accordion-item__title">{title}</h3>
-                    <span className="accordion-item__icon" />
-                </div>
-                <div className="accordion-item__inner">
-                    <div className="accordion-item__content">
-                        <p className="accordion-item__paragraph">{paragraph}</p>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+function Accordion({ match }) {
+    return (
+        <div className="wrapper">
+            <ul className="accordion-list">
+                {Object.keys(FAQData.data).map((k, idx) => {
+                    return (
+                        <li key={idx} {...{ className: 'accordion-list__item' }}>
+                            <AccordionItem title={FAQData.data[k].title} paragraph={FAQData.data[k].paragraph} />
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
 }
 
 export default Accordion;
--- src/pages/FAQ.js
+++ src/pages/FAQ.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/faq.scss';
 import Accordion from '../components/Accordion';
+import { Link, Route } from 'react-router-dom';
 
 function FAQ() {
     return (
@@ -10,8 +11,19 @@ function FAQ() {
                 <div className="kr desc-font">
                     원하는 질문이 없는 경우에는 하단의 이메일이나 카카오톡 플러스친구를 통해 문의 해주시기 바랍니다.
                 </div>
+
+                <div className="subRoute">
+                    <ul>
+                        <li>
+                            <Link to="/faq/01">지원 관련</Link>
+                        </li>
+                        <li>
+                            <Link to="/faq/02">스터디 진행</Link>
+                        </li>
+                    </ul>
+                </div>
                 <div className="accordion">
-                    <Accordion />
+                    <Route path="/faq/:01" component={Accordion}></Route>
                 </div>
             </div>
         </div>
