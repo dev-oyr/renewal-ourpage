3013ae1ea02fbc136b2b80e0bc8d7e61dc2a89bc
eviloliver <evil_oliver@kakao.com>
2020-02-28 18:08:33 +0900

    컴포넌트가 아예 사라지는 버그가 있음

--- src/components/LoginPanel.js
+++ src/components/LoginPanel.js
@@ -1,22 +1,25 @@
 import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 import '../styles/login.scss';
 
 const CssTextField = withStyles({
     root: {
         '& label.Mui-focused': {
-            color: 'black', //눌렀을때 색 어떤걸로 할지 정하기
+            color: '#333', //눌렀을때 색 어떤걸로 할지 정하기
         },
         '&label.Mui-error': {
             color: '#ff6d70',
         },
         '& .MuiOutlinedInput-root': {
             '& fieldset': {},
-            '&:hover fieldset': {},
+            '&:hover fieldset': {
+                borderColor: '#333',
+            },
             '&.Mui-focused fieldset': {
-                borderColor: 'black',
+                borderColor: '#333',
             },
             '&.Mui-error fieldset': {
                 borderColor: '#ff6d70',
@@ -59,12 +62,39 @@ function LoginPannel() {
         password: '',
         checkId: false,
         checkPassword: false,
+        open: false,
     });
+
+    let [open, setOpen] = React.useState({
+        message: '',
+        open: false,
+    });
+
     const handleChange = e => {
         const name = e.target.name;
         const value = e.target.value;
         account[name] = value;
         setAccount(account);
+    };
+
+    const handleClick = () => {
+        if (account.checkId == true) {
+            open.message = '존재하지 않는 아이디입니다.';
+        } else {
+            if (account.checkPassword == true) {
+                open.message = '비밀번호가 일치하지 않습니다.';
+            }
+        }
+        open.open = true;
+        setOpen({ ...open });
+    };
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        open.open = false;
+        setOpen({ ...open });
     };
     const loginCheck = () => {
         if (account.id.length % 2) {
@@ -81,6 +111,7 @@ function LoginPannel() {
         setAccount({
             ...account,
         });
+        handleClick();
         console.log(account);
     };
 
@@ -114,7 +145,15 @@ function LoginPannel() {
                 <Button fullWidth variant="contained" color="primary" className={classes.login_button} onClick={loginCheck}>
                     로그인
                 </Button>
-                <p>아이디 비밀번호 찾기 => Link로 하기</p>
+                <Snackbar
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                    }}
+                    open={open.open}
+                    autoHideDuration={6000}
+                    onClose={handleClose}
+                    message={open.message}
+                />
             </form>
         </div>
     );
