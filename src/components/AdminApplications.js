18007f06bcea5f2b6b7aa43b90acb7c86b10d30d
Sain-Tech <chyh1900@gmail.com>
2020-03-07 04:19:00 +0800

    데이터 불러오기...

--- src/components/AdminApplications.js
+++ src/components/AdminApplications.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { IconButton, Menu, MenuItem, Fade } from '@material-ui/core';
+import { FormatListBulleted } from '@material-ui/icons';
+import { dbCtrl } from '../database/DBCtrl';
 
 function AdminApplications() {
-    return <>지원서 조회</>;
+    const drawerWidth = 240;
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+
+    const handleClick = e => {
+        setAnchorEl(e.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+
+    const handleDrawerToggle = () => {
+        setMobileOpen(!mobileOpen);
+    };
+
+    return (
+        <>
+            <IconButton aria-label="showlist" aria-controls="applicants" aria-haspopup="true" onClick={handleClick}>
+                <FormatListBulleted />
+            </IconButton>
+            <Menu id="applicants" anchorEl={anchorEl} keepMounted open={open} onClose={handleClose} TransitionComponent={Fade}>
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+                {dbCtrl.getAllApplications('2020-1', {
+                    onSuccess(res) {
+                        Object.keys(res).map((key, id) => (
+                            <MenuItem onClick={handleClose} key={id}>
+                                {key} {res[key].name}
+                            </MenuItem>
+                        ));
+                    },
+                })}
+            </Menu>
+        </>
+    );
 }
 
 export default AdminApplications;
--- src/pages/Admins.js
+++ src/pages/Admins.js
@@ -1,26 +1,8 @@
 import React, { useState } from 'react';
-import { Redirect, NavLink } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
-import Box from '@material-ui/core/Box';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Divider from '@material-ui/core/Divider';
-import Drawer from '@material-ui/core/Drawer';
-import Hidden from '@material-ui/core/Hidden';
-import IconButton from '@material-ui/core/IconButton';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import MailIcon from '@material-ui/icons/Mail';
-import MenuIcon from '@material-ui/icons/Menu';
+import { Redirect } from 'react-router-dom';
 import Tab from '@material-ui/core/Tab';
 import Tabs from '@material-ui/core/Tabs';
 import TabPanel from '../components/TabPanel';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
 
 const checkAdmin = () => {
     const session = JSON.parse(sessionStorage.getItem('currentUser'));
