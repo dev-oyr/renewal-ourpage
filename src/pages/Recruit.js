ddef2601db1445c86636746283d1556b57624f23
eviloliver <evil_oliver@kakao.com>
2020-03-08 17:24:40 +0900

    모집안내 수정

--- src/pages/Recruit.js
+++ src/pages/Recruit.js
@@ -114,7 +114,7 @@ function Recruit() {
             <div className="target">
                 <div className="kr header-font">모집대상</div>
                 <div className="kr desc-font">
-                    학년, 학과, 학번, 성별, 인종(중국인 제외) 상관없이 웹 프로그래밍에 관심이 있고 열정이 쏟을 수 있는 사람이라면 누구나
+                    학년, 학과, 학번, 성별, 국적 상관없이 웹 프로그래밍에 관심이 있고 열정이 쏟을 수 있는 사람이라면 누구나
                 </div>
                 <div className="background-cover recruit"></div>
             </div>
