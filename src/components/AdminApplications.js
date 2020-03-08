301e6ca18de56d704e4c1cbe666dba74e351ee6d
Sain <chyh1900@gmail.com>
2020-03-08 22:08:49 +0900

    지원서 뷰어 개선

--- src/components/AdminApplications.js
+++ src/components/AdminApplications.js
@@ -1,10 +1,9 @@
-import React, { useState, Fragment, useEffect } from 'react';
-import { Chip, IconButton, Menu, MenuItem, Fade, Grid, TextField, Typography } from '@material-ui/core';
-import { FormatListBulleted } from '@material-ui/icons';
+import React, { useState, useEffect } from 'react';
+import { Chip, IconButton, Menu, MenuItem, Fade, Grid, TextField, Typography, List, ListItem, ListItemText } from '@material-ui/core';
 import { dbCtrl } from '../database/DBCtrl';
+import '../styles/adminapplications.scss';
 
 function AdminApplications() {
-    const drawerWidth = 240;
     const [anchorEl, setAnchorEl] = useState(null);
     const open = Boolean(anchorEl);
 
@@ -18,11 +17,6 @@ function AdminApplications() {
         setSelection({ stdNo: stdNo, ...applicants[stdNo] });
         setAnchorEl(null);
         console.log(selection);
-    };
-
-    const [mobileOpen, setMobileOpen] = React.useState(false);
-    const handleDrawerToggle = () => {
-        setMobileOpen(!mobileOpen);
     };
 
     const [applicants, setApplicants] = React.useState({});
@@ -40,35 +34,63 @@ function AdminApplications() {
         });
     }, []);
 
-    const selectApplicant = selection => {};
+    const [selectedStdNo, setSelectedStdNo] = React.useState('');
+
+    const handleListItemClick = (event, stdNo) => {
+        setSelectedStdNo(stdNo);
+        setSelection({ stdNo: stdNo, ...applicants[stdNo] });
+    };
 
     return (
-        <>
+        <div className="root-container">
             {console.log('check rerendering')}
-            <div>
-                <IconButton aria-label="showlist" aria-controls="applicants" aria-haspopup="true" onClick={handleClick}>
-                    <FormatListBulleted />
-                </IconButton>
-                <Menu id="applicants" anchorEl={anchorEl} keepMounted open={open} onClose={handleClose} TransitionComponent={Fade}>
-                    {Object.keys(applicants).map((key, id) => (
-                        <MenuItem onClick={handleClose} data-stdno={key} key={key}>
-                            {key} {applicants[key].name}
-                        </MenuItem>
-                    ))}
-                </Menu>
+            <div className="applicant-list">
+                <div className="mobile-btn">
+                    <IconButton
+                        aria-label="showlist"
+                        aria-controls="applicants"
+                        aria-haspopup="true"
+                        onClick={handleClick}
+                        style={{ margin: '24px 0 8px -12px', fontFamily: 'Noto Sans KR, sans-serif' }}
+                    >
+                        {selection.stdNo ? selection.stdNo : '지원자를 선택해 주세요!'} {selection.name ? selection.name : ''}
+                    </IconButton>
+                    <Menu id="applicants" anchorEl={anchorEl} keepMounted open={open} onClose={handleClose} TransitionComponent={Fade}>
+                        {Object.keys(applicants).map((key, id) => (
+                            <MenuItem onClick={handleClose} data-stdno={key} key={key}>
+                                {key} {applicants[key].name}
+                            </MenuItem>
+                        ))}
+                    </Menu>
+                </div>
+                <div className="normal-list">
+                    <List component="nav" aria-label="main mailbox folders">
+                        {Object.keys(applicants).map((key, id) => (
+                            <ListItem
+                                button
+                                data-stdno={key}
+                                key={key}
+                                selected={selectedStdNo === key}
+                                onClick={event => handleListItemClick(event, key)}
+                            >
+                                <ListItemText primary={applicants[key].name} secondary={key} />
+                            </ListItem>
+                        ))}
+                    </List>
+                </div>
             </div>
 
-            <div>
-                <Typography variant="subtitle1" gutterBottom>
+            <div className="appdatas">
+                <Typography variant="h6" style={{ fontFamily: 'Noto Sans KR, sans-serif' }}>
                     기본 정보
                 </Typography>
-                <Grid container spacing={2}>
+                <Grid container spacing={2} style={{ margin: '0 0 36px 0' }}>
                     <Grid item xs={12}>
                         <TextField
                             value={selection.name ? selection.name : ''}
                             id="v-name"
                             label="이름"
-                            style={{ margin: 8 }}
+                            style={{ margin: 6 }}
                             fullWidth
                             margin="normal"
                             size="small"
@@ -79,7 +101,7 @@ function AdminApplications() {
                             value={selection.birthday ? selection.birthday : ''}
                             id="v-birthday"
                             label="생년월일"
-                            style={{ margin: 8 }}
+                            style={{ margin: 6 }}
                             fullWidth
                             margin="normal"
                             size="small"
@@ -90,7 +112,7 @@ function AdminApplications() {
                             value={selection.gender ? selection.gender : ''}
                             id="v-gender"
                             label="성별"
-                            style={{ margin: 8 }}
+                            style={{ margin: 6 }}
                             fullWidth
                             margin="normal"
                             size="small"
@@ -101,7 +123,7 @@ function AdminApplications() {
                             value={selection.phone ? selection.phone : ''}
                             id="v-phone"
                             label="연락처"
-                            style={{ margin: 8 }}
+                            style={{ margin: 6 }}
                             fullWidth
                             margin="normal"
                             size="small"
@@ -112,7 +134,7 @@ function AdminApplications() {
                             value={selection.email ? selection.email : ''}
                             id="v-email"
                             label="이메일"
-                            style={{ margin: 8 }}
+                            style={{ margin: 6 }}
                             fullWidth
                             margin="normal"
                             size="small"
@@ -123,7 +145,7 @@ function AdminApplications() {
                             value={selection.stdNo ? selection.stdNo : ''}
                             id="v-stdno"
                             label="학번"
-                            style={{ margin: 8 }}
+                            style={{ margin: 6 }}
                             fullWidth
                             margin="normal"
                             size="small"
@@ -134,7 +156,7 @@ function AdminApplications() {
                             value={selection.major ? selection.major : ''}
                             id="v-major"
                             label="학과"
-                            style={{ margin: 8 }}
+                            style={{ margin: 6 }}
                             fullWidth
                             margin="normal"
                             size="small"
@@ -145,24 +167,24 @@ function AdminApplications() {
                             value={selection.grade ? selection.grade : ''}
                             id="v-grade"
                             label="학년/학기"
-                            style={{ margin: 8 }}
+                            style={{ margin: 6 }}
                             fullWidth
                             margin="normal"
                             size="small"
                         />
                     </Grid>
                 </Grid>
-                <Typography variant="subtitle1" gutterBottom>
+                <Typography variant="h6" style={{ fontFamily: 'Noto Sans KR, sans-serif' }}>
                     자기 소개
                 </Typography>
-                <Grid container spacing={2}>
+                <Grid container spacing={2} style={{ margin: '0 0 36px 0' }}>
                     <Grid item xs={12}>
                         <TextField
                             value={selection.motive ? selection.motive : ''}
                             id="v-motive"
                             label="지원 동기"
                             multiline
-                            style={{ margin: 8 }}
+                            style={{ margin: 6 }}
                             fullWidth
                             margin="normal"
                             size="small"
@@ -174,7 +196,7 @@ function AdminApplications() {
                             id="v-introduce"
                             label="자기 소개"
                             multiline
-                            style={{ margin: 8 }}
+                            style={{ margin: 6 }}
                             fullWidth
                             margin="normal"
                             size="small"
@@ -186,23 +208,23 @@ function AdminApplications() {
                             id="v-mywish"
                             label="만들고 싶은 웹 서비스"
                             multiline
-                            style={{ margin: 8 }}
+                            style={{ margin: 6 }}
                             fullWidth
                             margin="normal"
                             size="small"
                         />
                     </Grid>
                 </Grid>
-                <Typography variant="subtitle1" gutterBottom>
+                <Typography variant="h6" style={{ fontFamily: 'Noto Sans KR, sans-serif' }}>
                     프로젝트 경험
                 </Typography>
-                <Grid container spacing={2}>
+                <Grid container spacing={2} style={{ margin: '0 0 36px 0' }}>
                     <Grid item xs={12}>
                         <TextField
                             value={selection.projName ? selection.projName : ''}
                             id="v-projname"
                             label="프로젝트 이름"
-                            style={{ margin: 8 }}
+                            style={{ margin: 6 }}
                             fullWidth
                             margin="normal"
                             size="small"
@@ -214,7 +236,7 @@ function AdminApplications() {
                             id="v-projsummary"
                             label="프로젝트 요약"
                             multiline
-                            style={{ margin: 8 }}
+                            style={{ margin: 6 }}
                             fullWidth
                             margin="normal"
                             size="small"
@@ -226,7 +248,7 @@ function AdminApplications() {
                             id="v-projdetail"
                             label="프로젝트 상세 설명"
                             multiline
-                            style={{ margin: 8 }}
+                            style={{ margin: 6 }}
                             fullWidth
                             margin="normal"
                             size="small"
@@ -242,7 +264,7 @@ function AdminApplications() {
                             value={selection.projGithub ? selection.projGithub : ''}
                             id="v-projgithub"
                             label="GitHub"
-                            style={{ margin: 8 }}
+                            style={{ margin: 6 }}
                             fullWidth
                             margin="normal"
                             size="small"
@@ -250,10 +272,11 @@ function AdminApplications() {
                     </Grid>
                     <Grid item xs={12}>
                         <TextField
+                            href="www.github.com"
                             value={selection.projOtherLink ? selection.projOtherLink : ''}
                             id="v-projotherlink"
                             label="추가 링크"
-                            style={{ margin: 8 }}
+                            style={{ margin: 6 }}
                             fullWidth
                             margin="normal"
                             size="small"
@@ -261,7 +284,7 @@ function AdminApplications() {
                     </Grid>
                 </Grid>
             </div>
-        </>
+        </div>
     );
 }
 
